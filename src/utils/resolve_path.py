c2b1d59ad3d62cfd235081d735d02d5a427871ab
Clint Bryan Nguena <clint.bryan.nguena@mni.thm.de>
2026-09-04 16:20:57 +0200

    fix: remove print command in the resolve_path.py

--- src/utils/resolve_path.py
+++ src/utils/resolve_path.py
@@ -5,9 +5,7 @@ def resolve_project_path(relative_path: str) -> Path:
     current = Path(__file__).resolve()
 
     for parent in [current, *current.parents]:
-        print(parent/relative_path)
         if (parent/".checkpoint").exists():
-            print(parent/relative_path)
             return parent/relative_path
 
     raise RuntimeError("Project root could not be determined.")
